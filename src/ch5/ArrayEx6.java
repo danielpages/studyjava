bbfead81c2809976768a638851f7f531dac3132f
Daniel Park <pkhabkw@naver.com>
2020-01-04 18:48:19 +0900

    2020/01/04

---
+++ src/ch5/ArrayEx6.java
@@ -0,0 +1,24 @@
+package ch5;
+
+public class ArrayEx6 {
+
+	public static void main(String[] argrs) {
+		int[] score = {79, 88, 91, 33, 100, 55, 95};
+		
+		int max = score[0];
+		int min = score[0];
+		
+		for(int i = 0; i <score.length; i++) {
+			if(score[i] > max) {
+				max = score[i];
+			} else if(score[i] < min) {
+				min = score[i];
+			}
+		}
+		
+		System.out.println("Maximum : " + max);
+		System.out.println("minimum : " + min);
+
+	}
+
+}
