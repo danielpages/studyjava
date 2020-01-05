2a78374021aa6204382e36d8f5bb293e1f6bba68
Daniel Park <pkhabkw@naver.com>
2020-01-05 23:22:19 +0900

    2020/01/05

---
+++ src/ch5/ArrayEx16.java
@@ -0,0 +1,15 @@
+package ch5;
+
+public class ArrayEx16 {
+
+	public static void main(String[] args) {
+		System.out.println("NUmber of Parameter : " + args.length);
+		
+		for(int i = 0; i < args.length; i++) {
+			System.out.println("args[" + i + "] = \"" + args[i] + "\"");
+		}
+		
+
+	}
+
+}
